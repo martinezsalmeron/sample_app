728230b1093e6029a475a812d713b452f33f53e5
Ramon Martinez <martinezsalmeron@gmail.com>
2014-08-13 18:11:01 +0200

    Added controller Users

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "LayoutLinks", :type => :request do
   
-  it "should have a home page at '/' " do
-    get '/'
-    response.should have_selector('title', :content => "Home")
-  end
+  it "should have right links on the layout" do
+  visit root_path
+  response.should have_selector('title', :content => "Home")
+  end 
 
 end
